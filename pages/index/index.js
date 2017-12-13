25629a895ceae05346368c2c9dc5f17d41bd37f6
Chen-T <1432027069@qq.com>
2017-12-13 18:28:02 +0800

    首页JS文件上传，模板数据和照片url
    传入相应的模板数据和图片的网络地址，注意模板使用时传入str时格式问题，别忘记写''

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,4 @@
-// pages/index/index.js
+// pages/first/first.js
 Page({
 
   /**
@@ -6,9 +6,9 @@ Page({
    */
   data: {
     imgUrls: [
-"https://github.com/Chen-T/hello-world/blob/master/images/index/section01.png?raw=true",
-"https://github.com/Chen-T/hello-world/blob/master/images/index/section02.png?raw=true",
-"https://github.com/Chen-T/hello-world/blob/master/images/index/section03.png?raw=true"
+      "https://github.com/Chen-T/hello-world/blob/master/images/index/section01.png?raw=true",
+      "https://github.com/Chen-T/hello-world/blob/master/images/index/section02.png?raw=true",
+      "https://github.com/Chen-T/hello-world/blob/master/images/index/section03.png?raw=true"
     ],
     navigation_img: [
       "../images/index_icon/index_icon01.png",
@@ -16,12 +16,53 @@ Page({
       "../images/index_icon/index_icon03.png",
       "../images/index_icon/index_icon04.png"
     ],
-    
     indicatorDots: true,
     duration: 500,
     autoplay: true,
     interval: 5000,
-    indicatorActive: "#66B3FF"
+    indicatorActive: "#66B3FF",
+  
+    /*模板数据 */
+    module01: {
+      moduleTitle: "小社群"
+    },
+    module02: {
+      moduleTitle: "频道"
+    },
+    module03: {
+      moduleTitle: "最新"
+    },
+    soliciting: {
+      group_image: "https://github.com/Chen-T/hello-world/blob/master/images/index/group01.png?raw=true",
+      groupType: "求租",
+      description01: "求合租",
+      description02:" 招室友整租",
+      value01: 536,
+      value02: 214
+    },
+    ridicule:{
+      group_image: "https://github.com/Chen-T/hello-world/blob/master/images/index/group02.png?raw=true",
+      groupType: "吐槽",
+      description01: "黑中介",
+      description02: " 黑房东登记",
+      value01: 107,
+      value02: 15
+    },
+    rent:{
+      group_image: "https://github.com/Chen-T/hello-world/blob/master/images/index/group03.png?raw=true",
+      groupType: "出租",
+      description01: "房东直租",
+      description02: " 个人转租",
+      value01: 378,
+      value02: 87
+    },
+
+    rental_information:{
+      channel_image:"https://github.com/Chen-T/hello-world/blob/master/images/index/channel01.png?raw=true",
+      channel_title:"【北京地铁】沿线真实租房情况分享'",
+      channel_description:"首都租客联盟发起“北京·租客计划”，众多盟友响应，参与。联盟现将盟友提供的真实租房情",
+      channel_total:"5"
+    }
   },
   /**
    * 生命周期函数--监听页面加载
