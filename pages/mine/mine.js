69102de659ef483ccb1423f2cb56815abf328528
Chen-T <1432027069@qq.com>
2018-01-24 23:12:31 +0800

    上传‘我的’js

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,27 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-    personal_information:{
-      item_images:"../images/mine_icon/icon_Profile_Edit.png",
-      mine_description:"个人信息",
-      mine_go:'../images/mine_icon/go.png'
+    /*头像模块数据*/ 
+    mine_background:"https://github.com/Chen-T/hello-world/blob/master/images/mine/mine_background.JPG?raw=true",
+
+    /*条目数据*/
+    personal_information: {
+      item_images: "../images/mine_icon/icon_Profile_Edit.png",
+      mine_description: "个人信息",
+      mine_go: '../images/mine_icon/go.png'
     },
-    my_income:{
+    my_income: {
       item_images: "../images/mine_icon/icon_profile.png",
       mine_description: "我的收益",
       mine_go: '../images/mine_icon/go.png'
     },
-    promotion_center:{
+    promotion_center: {
       item_images: "../images/mine_icon/icon_Promotion.png",
       mine_description: "推广中心",
       mine_go: '../images/mine_icon/go.png'
     },
-    my_activities:{
+    my_activities: {
       item_images: "../images/mine_icon/icon_Profile_activity.png",
       mine_description: "我的活动",
       mine_go: '../images/mine_icon/go.png'
     },
-    my_quesAnser:{
+    my_quesAnser: {
       item_images: "../images/mine_icon/icon_questions.png",
       mine_description: "我的问答",
       mine_go: '../images/mine_icon/go.png'
@@ -42,12 +46,12 @@ Page({
     },
     coupons: {
       item_images: "../images/mine_icon/icon_coupons.png",
-      mine_description: "优惠券",
+      mine_description: "优惠券   ",
       mine_go: '../images/mine_icon/go.png'
     },
-    invitation_code:{
+    invitation_code: {
       item_images: "../images/mine_icon/icon_Profile_invite.png",
-      mine_description: "邀请码",
+      mine_description: "邀请码   ",
       mine_go: '../images/mine_icon/go.png'
     },
     feedback: {
@@ -61,55 +65,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
